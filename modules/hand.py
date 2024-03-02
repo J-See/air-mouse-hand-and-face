28488783f94730fac0e410b4165c7de8bfd0c103
J-See <sv961988@gmail.com>
2024-03-02 15:29:54 +0530

    removed unwanted comments

--- modules/hand.py
+++ modules/hand.py
@@ -5,11 +5,11 @@ import math
 
 def HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5):
     """
-    :param mode: In static mode, detection is done on each image: slower
-    :param maxHands: Maximum number of hands to detect
-    :param modelComplexity: Complexity of the hand landmark model: 0 or 1.
-    :param detectionCon: Minimum Detection Confidence Threshold
-    :param minTrackCon: Minimum Tracking Confidence Threshold
+    mode: In static mode, detection is done on each image: slower
+    maxHands: Maximum number of hands to detect
+    modelComplexity: Complexity of the hand landmark model: 0 or 1.
+    detectionCon: Minimum Detection Confidence Threshold
+    minTrackCon: Minimum Tracking Confidence Threshold
     """
     global mpHands, hands, mpDraw, tipIds, fingers, lmList
     mpHands = mp.solutions.hands
@@ -28,100 +28,10 @@ def HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0
 def findHands(img, draw=True, flipType=True):
     """
     Finds hands in a BGR image.
-    :param img: Image to find the hands in.
-    :param draw: Flag to draw the output on the image.
-    :return: Image with or without drawings
+    img: Image to find the hands in.
+    draw: Flag to draw the output on the image.
+    return: Image with or without drawings
     """
-    # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # global results, myHand
-    # results = hands.process(imgRGB)
-    # allHands = []
-    # h, w, c = img.shape
-
-    # ## left hand
-    # if results.left_hand_landmarks:
-    #     myHand = {}
-    #     ## lmList
-    #     mylmList = []
-    #     xList = []
-    #     yList = []
-    #     for id, lm in enumerate(results.left_hand_landmarks.landmark):
-    #         px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
-    #         mylmList.append([px, py, pz])
-    #         xList.append(px)
-    #         yList.append(py)
-
-    #     ## bbox
-    #     xmin, xmax = min(xList), max(xList)
-    #     ymin, ymax = min(yList), max(yList)
-    #     boxW, boxH = xmax - xmin, ymax - ymin
-    #     bbox = xmin, ymin, boxW, boxH
-    #     cx, cy = bbox[0] + (bbox[2] // 2), bbox[1] + (bbox[3] // 2)
-    #     myHand["lmList"] = mylmList
-    #     myHand["bbox"] = bbox
-    #     myHand["center"] = (cx, cy)
-
-    #     if flipType:
-    #         if results.left_hand_landmarks:
-    #             myHand["type"] = "Left"
-    #         else:
-    #             myHand["type"] = "Right"
-    #     else:
-    #         myHand["type"] = "Right"
-    #     allHands.append(myHand)
-
-    #     ## draw
-    #     if draw:
-    #         mpDraw.draw_landmarks(img, results.left_hand_landmarks, mpHands.HAND_CONNECTIONS)
-    #         cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-    #                         (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
-    #                         (255, 0, 255), 2)
-    #         cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
-    #                     2, (255, 0, 255), 2)
-
-    # ## right hand
-    # if results.right_hand_landmarks:
-    #     myHand = {}
-    #     ## lmList
-    #     mylmList = []
-    #     xList = []
-    #     yList = []
-    #     for id, lm in enumerate(results.right_hand_landmarks.landmark):
-    #         px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
-    #         mylmList.append([px, py, pz])
-    #         xList.append(px)
-    #         yList.append(py)
-
-    #     ## bbox
-    #     xmin, xmax = min(xList), max(xList)
-    #     ymin, ymax = min(yList), max(yList)
-    #     boxW, boxH = xmax - xmin, ymax - ymin
-    #     bbox = xmin, ymin, boxW, boxH
-    #     cx, cy = bbox[0] + (bbox[2] // 2), bbox[1] + (bbox[3] // 2)
-    #     myHand["lmList"] = mylmList
-    #     myHand["bbox"] = bbox
-    #     myHand["center"] = (cx, cy)
-
-    #     if flipType:
-    #         if results.right_hand_landmarks:
-    #             myHand["type"] = "Right"
-    #         else:
-    #             myHand["type"] = "Left"
-    #     else:
-    #         myHand["type"] = "Left"
-    #     allHands.append(myHand)
-
-    #     ## draw
-    #     if draw:
-    #         mpDraw.draw_landmarks(img, results.right_hand_landmarks, mpHands.HAND_CONNECTIONS)
-    #         cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-    #                         (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
-    #                         (255, 0, 255), 2)
-    #         cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
-    #                     2, (255, 0, 255), 2)
-
-    # return allHands, img
-    
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     global results, myHand
     results = hands.process(imgRGB)
@@ -185,36 +95,10 @@ def findHands(img, draw=True, flipType=True):
 
 
 def fingersUp(myHand):
-    # fingers = []
-    # myHandType = myHand["type"]
-    # myLmList = myHand["lmList"]
-
-    # if (results.left_hand_landmarks) or (results.right_hand_landmarks):
-    #     # Thumb
-    #     if myHandType == "Right":
-    #         if myLmList[tipIds[0]][0] > myLmList[tipIds[0] - 1][0]:
-    #             fingers.append(1)
-    #         else:
-    #             fingers.append(0)
-    #     else:
-    #         if myLmList[tipIds[0]][0] < myLmList[tipIds[0] - 1][0]:
-    #             fingers.append(1)
-    #         else:
-    #             fingers.append(0)
-
-    #     # 4 Fingers
-    #     for id in range(1, 5):
-    #         if myLmList[tipIds[id]][1] < myLmList[tipIds[id] - 2][1]:
-    #             fingers.append(1)
-    #         else:
-    #             fingers.append(0)
-
-
-    # return fingers
     """
     Finds how many fingers are open and returns in a list.
     Considers left and right hands separately
-    :return: List of which fingers are up
+    return: List of which fingers are up
     """
     fingers = []
     myHandType = myHand["type"]
@@ -242,26 +126,14 @@ def fingersUp(myHand):
     return fingers
 
 def findDistance(p1, p2, img=None, color=(255, 0, 255), scale=5):
-    # x1, y1 = p1
-    # x2, y2 = p2
-    # cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-    # length = math.hypot(x2 - x1, y2 - y1)
-    # info = (x1, y1, x2, y2, cx, cy)
-    # if img is not None:
-    #     cv2.circle(img, (x1, y1), scale, color, cv2.FILLED)
-    #     cv2.circle(img, (x2, y2), scale, color, cv2.FILLED)
-    #     cv2.line(img, (x1, y1), (x2, y2), color, max(1, scale // 3))
-    #     cv2.circle(img, (cx, cy), scale, color, cv2.FILLED)
-
-    # return length, info, img
     """
     Find the distance between two landmarks input should be (x1,y1) (x2,y2)
-    :param p1: Point1 (x1,y1)
-    :param p2: Point2 (x2,y2)
-    :param img: Image to draw output on. If no image input output img is None
-    :return: Distance between the points
-                Image with output drawn
-                Line information
+    p1: Point1 (x1,y1)
+    p2: Point2 (x2,y2)
+    img: Image to draw output on. If no image input output img is None
+    return: Distance between the points
+            Image with output drawn
+            Line information
     """
 
     x1, y1 = p1
